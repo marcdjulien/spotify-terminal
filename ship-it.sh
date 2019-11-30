2e4079bd9b864a3f4ec6650ca389babf0aa7559b
Marc-Daniel Julien <marcdjulien@gmail.com>
2019-11-29 18:25:52 -0800

    Add script to upload to PyPi (test only for now)

---
+++ ship-it.sh
@@ -0,0 +1,5 @@
+python3 setup.py sdist bdist_wheel
+python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+echo "Removing files..."
+rm -rf build dist *egg-info*
+echo "Done!"
