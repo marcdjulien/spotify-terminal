aaa0af948c5f1d78698a142add82542273961f29
Marc-Daniel Julien <marcdjulien@gmail.com>
2019-11-30 13:22:59 -0800

    Include pdc34dll in the installation (for Windows)

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='spotify-terminal',  
-    version="0.12.0",
-    scripts=['spotify-terminal.py'] ,
+    name="spotify-terminal",  
+    version="0.12.1",
+    scripts=["spotify-terminal.py"] ,
     author="Marc-Daniel Julien",
     author_email="marcdjulien@gmail.com",
     description="Terminal Spotify application",
@@ -14,11 +14,11 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/marcdjulien/spotify-terminal",
     packages=setuptools.find_packages(),
-    package_data={"spotify_terminal": ['.*']},
+    package_data={"spotify_terminal": [".*", "pdc34dll/*"]},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"
     ],
-    install_requires=['requests']
+    install_requires=["requests"]
 )
